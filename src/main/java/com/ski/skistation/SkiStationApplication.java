ba05fa8b3898b3f397805c162581f32f46f8cfd2
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2023-09-28 11:29:19 +0100

    Create Spring Project

---
+++ src/main/java/com/ski/skistation/SkiStationApplication.java
@@ -0,0 +1,13 @@
+package com.ski.skistation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ski/skistation/SkiStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ski.skistation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
